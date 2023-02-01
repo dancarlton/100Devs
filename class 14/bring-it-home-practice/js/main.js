428daf98d57bab577b898524b8fdb8bfb11aa5e0
Dan Carlton <114847661+dancarlton@users.noreply.github.com>
2023-02-01 13:14:32 -0800

    Add files via upload

---
+++ class 14/bring-it-home-practice/js/main.js
@@ -0,0 +1,47 @@
+// *Variables*
+// Create a variable and console log the value
+var x=5 ;
+    if (x===5) {
+        x = 3;
+
+        console.log(x);
+    }
+    
+// Create a variable, add 10 to it, and alert the value
+
+var z=10 ;
+    if (z===25) {
+        z = 10;
+    }
+
+ alert(z + 10)
+
+// *Functions*
+// Create a function that subtracts 4 numbers and alerts the difference
+
+function subtractFour(num1, num2, num3, num4) {
+    alert( num1 - num2 - num3 - num4 )
+}
+subtractFour(10, 9, 8, 7)
+
+// Create a function that divides one number by another and returns the remainder
+
+function dividesRemainder(num1, num2) {
+    return ( num1 / num2 )
+    
+}
+    console.log(return)
+
+dividesRemainder (10, 5)
+
+// *Conditionals*
+// Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+
+function addTwoNumbers(num1, num2) {
+    if (sum > 50);
+    else (alert( 'Jumanji!'));
+}
+addTwoNumbers ( num1 + num2 )
+addTwoNumbers(20,31)
+
+// Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
---
+++ class 16/age-checker/js/main.js
@@ -0,0 +1,34 @@
+//Create a conditonal that checks their age
+//If under 16, tell them they can not drive
+//If under 18, tell them they can't hate from outside the club, because they can't even get in
+//If under 21, tell them they can not drink
+//If under 25, tell them they can not rent cars affordably
+//If under 30, tell them they can not rent fancy cars affordably
+//If under over 30, tell them there is nothing left to look forward too
+
+let age = 28
+
+if (age < 16) {
+    console.log('Can\'t drive')
+}else if (age < 18) {
+    console.log("You can't even get in the club!")
+}else if (age < 21) {
+    console.log("You cant drink too much")
+}else if (age < 25) {
+    console.log("You cant rent cars affordably")
+}else if (age < 30) {
+    console.log("You cant rent fancy cars affordable")
+}else if (age > 30) {
+    console.log("There is nothing left to look forward to")
+}
+
+//--- Harder
+//On click of the h1
+//Take the value from the input
+//Place the result of the conditional in the paragraph
+const h1Click = document.querySelector('h1')
+
+
+
+
+
