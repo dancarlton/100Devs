2bad7dcde12677b7ed157be078fa4ed633082ee3
Dan Carlton <danc@travelbugs.io>
2023-02-28 10:10:59 -0800

    Street Fighter Constructors

--- JS-Super-Review/super-review/objects-street-fighter/js/main.js
+++ JS-Super-Review/super-review/objects-street-fighter/js/main.js
@@ -1,1 +1,25 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+class StreetFighter {
+    constructor(name, health, speed, specialMove){
+        this.name = name
+        this.health = health
+        this.speed = speed
+        this.specialMove = specialMove
+
+    this.finishingMove = function(){
+        console.log(`My name is ${this.name} and your life is mine!!`)
+    }
+
+    this.taunt = function(){
+        console.log(`You can't handle my ${this.specialMove}`)
+    }
+
+    this.fatality = function(){
+        console.log(`Get over here!!`)
+    }
+
+}
+}
+
+let chunLi = new StreetFighter('Chun Li', 100, 100, 'Spinning Bird Kick')
