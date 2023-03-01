d0a09df0058c359e1ce04e06d2b0aec08cf3be1f
Dan Carlton <danc@travelbugs.io>
2023-02-28 22:10:10 -0800

    CRUD

--- CRUD-Tutorial/server.js
+++ CRUD-Tutorial/server.js
@@ -1,6 +1,7 @@
 console.log('May Node be with you');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.listen(3000, function() {
@@ -11,8 +12,19 @@ app.listen(3000, function() {
 //     res.send('Hello World')
 // })
 
+app.use(bodyParser.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-console.log(__dirname)
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
+
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect('mongodb-connection-string', (err, client) => {
+    //  ... do something here
+})
+
+// Could not get mongoDB working on the atlas website
